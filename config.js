1833aeb1e4955ba577f46f998ce5b54107f2e8b5
floriane-svg <andrieuxfloriane@gmail.com>
2025-12-16 20:44:14 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ module.exports = {
   urls: [
     {
       name: 'Ilha dos Caiçaras',
-      url: 'https://www.quintoandar.com.br/alugar/imovel/ilha-dos-caicaras-lagoa-rio-de-janeiro-rj-brasil/de-500-a-8500-reais/apartamento/kitnet/1-quartos',
+      url: 'https://www.quintoandar.com.br/alugar/imovel/ilha-dos-caicaras-lagoa-rio-de-janeiro-rj-brasil/de-500-a-3500-reais/apartamento/kitnet/1-quartos',
       threshold: 1
     },
     {
       name: 'Leblon',
-      url: 'https://www.quintoandar.com.br/alugar/imovel/leblon-rio-de-janeiro-rj-brasil/de-500-a-8500-reais/apartamento/kitnet/1-quartos',
+      url: 'https://www.quintoandar.com.br/alugar/imovel/leblon-rio-de-janeiro-rj-brasil/de-500-a-3500-reais/apartamento/kitnet/1-quartos',
       threshold: 1   // 👈 alerte à partir de 2 annonces
     },
     {
