e53ddfba3fd8bf507e1c3085c9f3c60e8dc3892c
floriane-svg <andrieuxfloriane@gmail.com>
2025-12-16 19:18:38 -0300

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ module.exports = {
     {
       name: 'Leblon',
       url: 'https://www.quintoandar.com.br/alugar/imovel/leblon-rio-de-janeiro-rj-brasil/de-500-a-3500-reais/apartamento/kitnet/1-quartos',
-      threshold: 2   // 👈 alerte à partir de 2 annonces
+      threshold: 1   // 👈 alerte à partir de 2 annonces
     },
     {
       name: 'Rua Dias Ferreira 417 - Leblon',
       url: 'https://www.quintoandar.com.br/alugar/imovel/rua-dias-ferreira-417-leblon-rio-de-janeiro-rj-brasil/de-500-a-3500-reais/apartamento/kitnet/1-quartos',
-      threshold: 2
+      threshold: 1
     }
   ]
 };
