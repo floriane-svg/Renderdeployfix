2fd594920f7fe1c3bc625ae0cd9a60379e3e898c
floriane-svg <andrieuxfloriane@gmail.com>
2025-12-23 18:53:51 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ module.exports = {
     {
       name: 'Ilha dos Caiçaras',
       url: 'https://www.quintoandar.com.br/alugar/imovel/ilha-dos-caicaras-lagoa-rio-de-janeiro-rj-brasil/de-500-a-3500-reais/apartamento/kitnet/1-quartos',
-      threshold: 2
+      threshold: 1
     },
     {
       name: 'Leblon',
       url: 'https://www.quintoandar.com.br/alugar/imovel/leblon-rio-de-janeiro-rj-brasil/de-500-a-3500-reais/apartamento/kitnet/1-quartos',
-      threshold: 2   // 👈 alerte à partir de 2 annonces
+      threshold: 1   // 👈 alerte à partir de 2 annonces
     },
     {
       name: 'Rua Dias Ferreira 417 - Leblon',
       url: 'https://www.quintoandar.com.br/alugar/imovel/rua-dias-ferreira-417-leblon-rio-de-janeiro-rj-brasil/de-500-a-3500-reais/apartamento/kitnet/1-quartos',
-      threshold: 2
+      threshold: 1
     }
   ]
 };
